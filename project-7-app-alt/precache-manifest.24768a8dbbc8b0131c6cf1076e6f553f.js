872a2baec986760f14b02b6a5b62cc82c634cd31
GenericName <frantisek.meciar@gmail.com>
2020-07-11 08:57:30 +0200

    Added new react project

---
+++ project-7-app-alt/precache-manifest.24768a8dbbc8b0131c6cf1076e6f553f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722cc4e86f01195fd91542287da9b053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e66a5495c99e12eceba",
+    "url": "/static/css/main.bd90be2c.chunk.css"
+  },
+  {
+    "revision": "2cfcca52b9c5715b0343",
+    "url": "/static/js/2.4d1b2c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d1b2c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e66a5495c99e12eceba",
+    "url": "/static/js/main.53cc8a21.chunk.js"
+  },
+  {
+    "revision": "bdb15bfe974e49255bad",
+    "url": "/static/js/runtime-main.10a1c9c4.js"
+  }
+]);
